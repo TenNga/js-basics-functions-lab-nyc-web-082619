51934755445fa63dd1afbe1cbe55291e756618d1
Kevin Karma <kevinkarma55@gmail.com>
2019-10-05 16:08:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue){
+  if (someValue > 42)
+    return someValue % 42;
+  else
+    return 42 % someValue;
+}
+
+function distanceFromHqInFeet(someValue){
+  return distanceFromHqInBlocks(someValue) * 264;
+}
+
+function distanceTravelledInFeet(start,end) {
+  return (end - start) * 264;
+}
